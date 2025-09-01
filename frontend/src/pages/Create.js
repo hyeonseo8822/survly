f5f5246c19c6a409384400641d9a17c3ed5e97ef
조현서 <hyeonseojo929@gmail.com>
2025-09-01 11:28:42 +0900

    Finish Create page and Navbar UI

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import './css/Create.css';
 import NavBar2 from '../components/NavBar2';
 
 function Create() {
+  const [active, setActive] = useState("question");
+
   return (
     <div className='container'>
       <div className='navBox'>
         <NavBar2 />
         <div className='select'>
-          <div className='question'>
+          <div className={`question ${active === "question" ? "selectActive" : ""}`} onClick={() => setActive("question")}>
             질문
           </div>
-          <div className='answer'>
+          <div className={`answer ${active === "answer" ? "selectActive" : ""}`} onClick={() => setActive("answer")}>
             응답
           </div>
         </div>
